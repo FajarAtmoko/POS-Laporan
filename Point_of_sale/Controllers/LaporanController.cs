589e742debe477abbb9c99e956f45b31ba8c0af2
Fajar Atmoko <fajaratmoko710@gmail.com>
2022-05-27 14:33:55 +0700

    add Laporan

---
+++ Point_of_sale/Controllers/LaporanController.cs
@@ -0,0 +1,29 @@
+﻿using Point_of_sale.Entitas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Point_of_sale.Controllers
+{
+    public class LaporanController : Controller
+    {
+        // GET: Laporan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Getlist()
+        {
+            using (DBModels db = new DBModels())
+            {
+                var empList = db.TB_Laporan.ToList<TB_Laporan>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
